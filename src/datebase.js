8656f7b3c682653f94b7947a1b9f7de8f850b052
David Rios <drriosg@unal.edu.co>
2023-05-21 20:31:37 -0500

    Cambios para despliegue

--- src/datebase.js
+++ src/datebase.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose'
 
 
 
-//mongoose.connect("mongodb+srv://jatoroe:tTZE19uT7JyX3gzf@usermanagement.uwk4w.mongodb.net/?retryWrites=true&w=majority/Users",{
-mongoose.connect("mongodb://digapp_auth_db:27017/Users",{
-    //writeConcern: { w: 1, wtimeout: 0, provenance: 'clientSupplied' }
+mongoose.connect("mongodb+srv://jatoroe:tTZE19uT7JyX3gzf@usermanagement.uwk4w.mongodb.net/?retryWrites=true&w=majority/Users",{
+//mongoose.connect("mongodb://digapp_auth_db:27017/Users",{
+    writeConcern: { w: 1, wtimeout: 0, provenance: 'clientSupplied' }
 })
 .then(db => console.log('DB is Connected'))
 .catch(error => console.log('MongooseError', error))
