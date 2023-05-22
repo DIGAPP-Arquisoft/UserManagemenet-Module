992471766b2c9160570c7b153d510a4920bcdd8d
David Rios <drriosg@unal.edu.co>
2023-05-21 20:30:16 -0500

    cambios para despliegue

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -16,11 +16,11 @@ export const signup = async (req, res) => {
     })
 
     const savedUser = await newUser.save()
-    const token = jwt.sign({id: savedUser._id}, config.SECRET, {
+    const Token = jwt.sign({id: savedUser._id}, config.SECRET, {
         expiresIn: 600 // 10 min
     })
 
-    res.status(200).json({token})
+    res.status(200).json({Token})
 }
 
 
@@ -28,16 +28,16 @@ export const signin = async (req, res) => {
 
     const userFound = await User.findOne({Email: req.body.Email})
     
-    if(!userFound) return res.status(400).json({message: 'User not Found'})
+    if(!userFound) return res.status(200).json({Message: 'User not Found'})
 
     const matchPassword = await User.comparePassword(req.body.Password, userFound.Password)
 
-    if(!matchPassword) return res.status(401).json({token: null, message: 'Invalid Password'})
+    if(!matchPassword) return res.status(200).json({Token: null, Message: 'Invalid Password'})
 
-    const token = jwt.sign({id: userFound._id}, config.SECRET, {
+    const Token = jwt.sign({id: userFound._id}, config.SECRET, {
         expiresIn: 600
     })
-    res.json({token})
+    res.json({Token})
 }
 
 export const verifyToken = async (req, res) => {
@@ -46,15 +46,15 @@ export const verifyToken = async (req, res) => {
         const token = req.headers["x-access-token"]
         console.log(token)
     
-        if(!token) return res.status(403).json({message: 'No token provided'})
+        if(!token) return res.status(403).json({Message: 'No token provided'})
     
         const decoded = jwt.verify(token, config.SECRET)
     
         const user = await User.findById(decoded.id, {Password: 0})
-        if (!user) return res.status(404).json({message: 'No user found'})
+        if (!user) return res.status(404).json({Message: 'No user found'})
     
         res.status(200).json(user)
     } catch {
-        return res.status(401).json({message: "unauthorized"})
+        return res.status(401).json({Message: "unauthorized"})
     }
 }
--- src/middlewares/verifySignup.js
+++ src/middlewares/verifySignup.js
@@ -2,9 +2,9 @@ import User from "../models/User.js"
 
 export const verifyUserExist = async (req, res, next) => {
     const user = await User.findOne({UserName: req.body.UserName})
-    if (user) return res.status(400).json({message: 'The user already exists'})
+    if (user) return res.status(200).json({Message: 'The user already exists'})
     const email = await User.findOne({Email: req.body.Email})
-    if (email) return res.status(400).json({message: 'The email already exists'})
+    if (email) return res.status(200).json({Message: 'The email already exists'})
 
     next()
 }
