bd303133ec5f7d0c9a1313d9362a37fb90ef81e5
Juanse Vargas MBP16 <jsvargasc@unal.edu.co>
2023-06-21 00:35:51 -0500

    :bug: Trying to fix bug

--- src/datebase.js
+++ src/datebase.js
@@ -1,9 +1,14 @@
 import mongoose from 'mongoose'
 
+// mongoose.connect("mongodb+srv://jatoroe:tTZE19uT7JyX3gzf@usermanagement.uwk4w.mongodb.net/?retryWrites=true&w=majority/Users",{
+// //mongoose.connect("mongodb://digapp_auth_db:27017/Users",{
+//     writeConcern: { w: 1, wtimeout: 0, provenance: 'clientSupplied' }
+// })
+// .then(db => console.log('DB is Connected'))
+// .catch(error => console.log('MongooseError', error))
 
-
-mongoose.connect("mongodb+srv://jatoroe:tTZE19uT7JyX3gzf@usermanagement.uwk4w.mongodb.net/?retryWrites=true&w=majority/Users",{
-//mongoose.connect("mongodb://digapp_auth_db:27017/Users",{
+//mongoose.connect("mongodb+srv://jatoroe:tTZE19uT7JyX3gzf@usermanagement.uwk4w.mongodb.net/?retryWrites=true&w=majority/Users",{
+mongoose.connect("mongodb://10.142.0.2:27017/Users",{
     writeConcern: { w: 1, wtimeout: 0, provenance: 'clientSupplied' }
 })
 .then(db => console.log('DB is Connected'))
