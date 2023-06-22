9212a4b97d91c531891e37885e0287de84b2ba69
Juanse Vargas MBP16 <jsvargasc@unal.edu.co>
2023-06-21 22:18:11 -0500

    :bug: Fixing ip bug

--- src/datebase.js
+++ src/datebase.js
@@ -8,7 +8,7 @@ import mongoose from 'mongoose'
 // .catch(error => console.log('MongooseError', error))
 
 //mongoose.connect("mongodb+srv://jatoroe:tTZE19uT7JyX3gzf@usermanagement.uwk4w.mongodb.net/?retryWrites=true&w=majority/Users",{
-mongoose.connect("mongodb://192.168.0.10:27017/Users",{
+mongoose.connect("mongodb://192.168.0.16:27017/Users",{
     writeConcern: { w: 1, wtimeout: 0, provenance: 'clientSupplied' }
 })
 .then(db => console.log('DB is Connected'))
